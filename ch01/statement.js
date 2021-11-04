da16eba81ecfca2715c91d30398eda252bc45496
ukdong <lklone20005@gmail.com>
2021-11-04 16:35:26 +0900

    divide statement(calculate) with renderPlainText(renderer)

--- ch01/statement.js
+++ ch01/statement.js
@@ -1,7 +1,30 @@
 const invoices = require('./invoices.json')
 const plays = require('./plays.json')
 
+/*
+  목표 - statement()의 HTML 버전을 만들자.
+  문제 - 기존의 statement()에서 계산코드를 모두 분리해내었지만 중첩함수이다.
+        이 함수들을 모두 그대로 복사하여 HTML버전을 만드는 데에 중복해서 넣고싶지 않다.
+        일반 TEXT 버전과 HTML 모두 동일한 계산함수들을 사용하고 싶다.
+  해결 - 단계를 쪼개자. statement()에서 필요한 데이터에 대한 계산을 하고 이 결과를 TEXT or HTML로 표현하면 될 것 같다.
+*/
+
 function statement(invoice, plays) {
+  return renderPlainText(invoice, plays)
+}
+
+function renderPlainText(invoice, plays) {
+  let result = `청구 내역 (고객명: ${invoice[0].customer})\n`;
+
+  for (let perf of invoice[0].performances) {
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
+  }
+
+  result += `총액: ${usd(totalAmount())}\n`
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`
+
+  return result
+
   function usd(aNumber) {
     return new Intl.NumberFormat("es-US", {
       style: "currency",
@@ -9,7 +32,6 @@ function statement(invoice, plays) {
       minimumFractionDigits: 2
     }).format(aNumber / 100)
   }
-
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
@@ -18,11 +40,9 @@ function statement(invoice, plays) {
 
     return result
   }
-
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
-
   function amountFor(aPerformance) { // 값이 바뀌지 않는 변수는 매개변수로 전달
     let result = 0; // 변수를 초기화 && 명확한 이름으로 변경
 
@@ -43,7 +63,6 @@ function statement(invoice, plays) {
 
     return result
   }
-
   function totalVolumeCredits() {
     let result = 0;
 
@@ -53,7 +72,6 @@ function statement(invoice, plays) {
 
     return result
   }
-
   function totalAmount() {
     let result = 0;
 
@@ -63,17 +81,6 @@ function statement(invoice, plays) {
 
     return result
   }
-
-  let result = `청구 내역 (고객명: ${invoice[0].customer})\n`;
-
-  for (let perf of invoice[0].performances) {
-    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
-  }
-
-  result += `총액: ${usd(totalAmount())}\n`
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`
-
-  return result
 }
 
 const result = statement(invoices, plays)
