3682ebedf2cb8912522681752a8b629d7d9e6505
ukdong <lklone20005@gmail.com>
2021-11-04 18:03:50 +0900

    move totalAmount && totalVolumeCredits

--- ch01/statement.js
+++ ch01/statement.js
@@ -13,6 +13,9 @@ function statement(invoice, plays) {
   const statementData = {} // 중간 데이터
   statementData.customer = invoice.customer // 고객데이터를 중간데이터로
   statementData.performances = invoice.performances.map(enrichPerformance)
+  statementData.totalAmount = totalAmount(statementData)
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData)
+
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance)
@@ -57,6 +60,26 @@ function statement(invoice, plays) {
     return result
   }
 
+  function totalVolumeCredits(data) {
+    let result = 0;
+
+    for (let perf of data.performances) {
+      result += perf.volumeCredits;
+    }
+
+    return result
+  }
+
+  function totalAmount(data) {
+    let result = 0;
+
+    for (let perf of data.performances) {
+      result += perf.amount
+    }
+
+    return result
+  }
+
   return renderPlainText(statementData, plays)
 }
 
@@ -67,8 +90,8 @@ function renderPlainText(data, plays) { // 필요 없어진 invoice 삭제
     result += `${perf.play.name}: ${perf.amount} (${perf.audience}석)\n`
   }
 
-  result += `총액: ${usd(totalAmount())}\n`
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`
+  result += `총액: ${usd(data.totalAmount)}\n`
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`
 
   return result
 
@@ -81,25 +104,7 @@ function renderPlainText(data, plays) { // 필요 없어진 invoice 삭제
   }
 
 
-  function totalVolumeCredits() {
-    let result = 0;
 
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-
-    return result
-  }
-
-  function totalAmount() {
-    let result = 0;
-
-    for (let perf of data.performances) {
-      result += perf.amount
-    }
-
-    return result
-  }
 }
 
 const result = statement(invoices, plays)
