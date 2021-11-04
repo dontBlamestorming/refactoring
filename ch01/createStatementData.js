a1a89c190ff8dcb2d1bc13dbb206a403c86be2ea
ukdong <lklone20005@gmail.com>
2021-11-04 21:22:42 +0900

    inline amountFor

--- ch01/createStatementData.js
+++ ch01/createStatementData.js
@@ -17,7 +17,7 @@ export default function createStatementData(invoice, plays) {
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance))
     const result = Object.assign({}, aPerformance)
     result.play = calculator.play
-    result.amount = amountFor(result)
+    result.amount = calculator.amount
     result.volumeCredits = volumeCreditsFor(result)
 
     return result
@@ -25,10 +25,6 @@ export default function createStatementData(invoice, plays) {
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount
   }
 
   function volumeCreditsFor(aPerformance) {
