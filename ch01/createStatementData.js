d7b82a808861f75bc7dc4d0da3f8a93018bc9510
ukdong <lklone20005@gmail.com>
2021-11-05 00:58:02 +0900

    add subclass by type which is inherited super class

--- ch01/createStatementData.js
+++ ch01/createStatementData.js
@@ -5,7 +5,12 @@
 */
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay)
+  switch (aPlay.type) {
+    case "tragedy": return new TragedyCalculator(aPerformance, aPlay)
+    case "comedy": return new ComedyCalculator(aPerformance, aPlay)
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`)
+  }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -46,26 +51,8 @@ class PerformanceCalculator {
     this.play = aPlay
   }
 
-  get amount() { // <-- amountFor() 함수의 코드를 계산기 클래스로 복사
-    let result = 0;
-
-    switch (this.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (this.performance.audience > 30) result += 1000 * (this.performance.audience - 30)
-        break;
-
-      case "comedy":
-        result = 30000;
-        if (this.performance.audience > 20) result += 1000 + 500 * (this.performance.audience - 30)
-        result += 300 * this.performance.audience
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르: ${this.play.type}`)
-    }
-
-    return result
+  get amount() {
+    throw new Error('서브클래스에서 처리하도록 설계되었습니다.')
   }
 
   get volumeCredits() {
@@ -73,6 +60,29 @@ class PerformanceCalculator {
     result += Math.max(this.performance.audience - 30, 0);
 
     if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5)
+
+    return result
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30)
+    }
+
+    return result
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20)
+    }
+    result += 300 * this.performance.audience
 
     return result
   }
