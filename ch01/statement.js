1be10ea146ce801b6b5204695843a157f755a25d
ukdong <lklone20005@gmail.com>
2021-11-04 14:35:10 +0900

    add volumeCreditsFor function && 변수 이름 변경

--- ch01/statement.js
+++ ch01/statement.js
@@ -11,6 +11,15 @@ function statement(invoice, plays) {
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice[0].customer})\n`;
 
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+
+    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5)
+
+    return result
+  }
+
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
@@ -38,9 +47,9 @@ function statement(invoice, plays) {
 
   for (let perf of invoice[0].performances) {
     // 포인트 적립
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    volumeCredits += volumeCreditsFor(perf)
 
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5)
+    if ("comedy" === playFor(aPerformance).type) volumeCredits += Math.floor(perf.audience / 5)
 
     // 청구내역 출력
     result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`
