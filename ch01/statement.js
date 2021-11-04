8ad6e9fb3b89c39b24ae9e38be7845ccf6ff3680
ukdong <lklone20005@gmail.com>
2021-11-04 13:30:47 +0900

    extract amountFor function && change thisAmount to result

--- ch01/statement.js
+++ ch01/statement.js
@@ -1,6 +1,27 @@
 const invoices = require('./invoices.json')
 const plays = require('./plays.json')
 
+function amountFor(perf, play) { // 값이 바뀌지 않는 변수는 매개변수로 전달
+  let result = 0; // 변수를 초기화 && 명확한 이름으로 변경
+
+  switch (play.type) {
+    case "tragedy":
+      result = 40000;
+      if (perf.audience > 30) result += 1000 * (perf.audience - 30)
+      break;
+
+    case "comedy":
+      result = 30000;
+      if (perf.audience > 20) result += 1000 * (perf.audience - 30)
+      break;
+
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`)
+  }
+
+  return result
+}
+
 function statement(invoice, plays) {
   const format = new Intl.NumberFormat("es-US", {
     style: "currency",
@@ -13,23 +34,7 @@ function statement(invoice, plays) {
 
   for (let perf of invoice[0].performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) thisAmount += 1000 * (perf.audience - 30)
-
-        break;
-
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) thisAmount += 1000 * (perf.audience - 30)
-
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`)
-    }
+    let thisAmount = amountFor(perf, play) // 추출한 함수 이용
 
     // 포인트 적립
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -40,9 +45,9 @@ function statement(invoice, plays) {
     result += `${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`
     totalAmount += thisAmount
   }
+
   result += `총액: ${format(totalAmount / 100)}\n`
   result += `적립 포인트: ${volumeCredits}점\n`
-
 
   return result
 }
