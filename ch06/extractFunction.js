953b6430b5b7220607c0c086106a9f7998f5f91c
ukdong <lklone20005@gmail.com>
2021-12-29 23:53:45 +0900

    2 way of extract function

--- ch06/extractFunction.js
+++ ch06/extractFunction.js
@@ -1,9 +1,9 @@
 function printOwing(invoice) {
-  let outstanding = 0;
+  // 지역변수를 참조만 하는 것이 아니라 특정 값을 대입해야 한다면 다음을 따른다.
 
   printBanner()
 
-  // 미해결 채무 계산
+  let outstanding = 0; // #1 맨 위에 있던 선언문을 이 위치로 이동.
   for(const o of invoice.orders) {
     outstanding += o.amount
   }
@@ -11,6 +11,11 @@ function printOwing(invoice) {
   recordDueDate(invoice)
   printDetails(invoice, outstanding)
 }
+
+/*
+  이전 2개의 커밋과 같이 지역 변수에 다른 값을 대입하는 것이 아닌 참조(Read)만 하는 경우
+  매개변수로 넘겨서 활용해도 된다. 객체인 경우는 필드값을 수정할 수 있다(참조의 관계이므로).
+*/
 
 function recordDueDate(invoice) {
   const today = Clock.today
