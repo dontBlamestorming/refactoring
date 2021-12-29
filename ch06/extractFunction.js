98185c4a5533896837c87bb3b9262dda275be0fd
ukdong <lklone20005@gmail.com>
2021-12-29 23:59:02 +0900

    extract function complete

--- ch06/extractFunction.js
+++ ch06/extractFunction.js
@@ -1,29 +1,18 @@
 function printOwing(invoice) {
   // 지역변수를 참조만 하는 것이 아니라 특정 값을 대입해야 한다면 다음을 따른다.
-
   printBanner()
-
-  let outstanding = 0;
-  for(const o of invoice.orders) {
-    outstanding += o.amount
-  }
-
+  const outstanding = calculateOutstanding(invoice) // #3 함수 추출 완료. 추출한 함수가 반환한 값을 원래 변수에 저장
   recordDueDate(invoice)
   printDetails(invoice, outstanding)
 }
 
-/*
-  이전 2개의 커밋과 같이 지역 변수에 다른 값을 대입하는 것이 아닌 참조(Read)만 하는 경우
-  매개변수로 넘겨서 활용해도 된다. 객체인 경우는 필드값을 수정할 수 있다(참조의 관계이므로).
-*/
-
 function calculateOutstanding(invoice) {
-  let outstanding = 0; // #2 추출할 코드를 복사한다.
+  let result = 0;
   for(const o of invoice.orders) {
-    outstanding += o.amount
+    result += o.amount
   }
 
-  return outstanding // #2-1 수정된 값 반환
+  return result
 }
 
 function recordDueDate(invoice) {
