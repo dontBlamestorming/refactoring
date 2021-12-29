a96b19620bfa229e053c5abf14ad42fca234cb2a
ukdong <lklone20005@gmail.com>
2021-12-29 23:33:09 +0900

    make function with printBanner, printDetails

--- ch06/extractFunction.js
+++ ch06/extractFunction.js
@@ -1,9 +1,7 @@
 function printOwing(invoice) {
   let outstanding = 0;
 
-  console.log("***********")
-  console.log("* 고객 채무 *")
-  console.log("***********")
+  printBanner()
 
   // 미해결 채무 계산
   for(const o of invoice.orders) {
@@ -15,8 +13,19 @@ function printOwing(invoice) {
   console.log(today)
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30)
 
-  // 세부사항 출력
-  console.log(`고객명: ${invoice.customer}`)
-  console.log(`채무액: ${outstanding}`)
-  console.log(`마감일: ${invoice.dueDate.toLocaleTimeString()}`)
+
+  printDetails()
+
+  function printDetails() {
+    // 세부사항 출력
+    console.log(`고객명: ${invoice.customer}`)
+    console.log(`채무액: ${outstanding}`)
+    console.log(`마감일: ${invoice.dueDate.toLocaleTimeString()}`)
+  }
+}
+
+function printBanner() {
+  console.log("***********")
+  console.log("* 고객 채무 *")
+  console.log("***********")
 }
